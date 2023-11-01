3b5f6eedf24976934f5a008350b6c771ba090dcc
marian-novak <marian.novak.023@pslib.cz>
2023-11-01 09:38:15 +0100

    nová lekce

---
+++ lekce4-while.py
@@ -0,0 +1,13 @@
+#comamdelat = "nic"
+
+#while comamdelat != "skonci":
+#    print("Jeste nekoncim")
+#    comamdelat = input("Co mam delat?")
+
+comamdelat = "pokracuj"
+
+while comamdelat == "pokracuj":
+    print("pokracuju")
+    comamdelat = input("Co mam delat?")
+# != je nerovná se
+
