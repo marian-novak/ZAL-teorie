0e67cbd6ac26a0ae6d0aeffd7467024a591672fe
marian-novak <marian.novak.023@pslib.cz>
2023-11-20 15:22:33 +0100

    nov

--- Zadání ZAL-03.py
+++ Zadání ZAL-03.py
@@ -1,15 +1,21 @@
 from math import isclose
 from math import sqrt
 
-cislo1 = int(input("Zadejte M: "))
-cislo2 = int(input("Zadejte N: "))
+od = int(input("Zadejte M: "))
+do = int(input("Zadejte N: "))
 
-while cislo1 <= cislo2:
-    if cislo1 > 1:
-        i = 2
-        yesprvo = True
-        while i <= cislo2 - 1:
-            if cislo1 % i == 0:
-                yesprvo = False
-                break
-            
+cislo = od
+
+while cislo <= do:   
+   if cislo > 1:
+      i = 2
+      yesprvo = True
+      while i <= sqrt(cislo):
+           if cislo % i == 0:
+               yesprvo = False
+               break
+           
+           i = i + 1
+           if yesprvo == True:
+                    print(cislo)
+cislo = cislo + 1
