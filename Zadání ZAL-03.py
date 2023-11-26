5a5efeff19320497e44f8ebac93e94b2de8fcc46
marian-novak <marian.novak.023@pslib.cz>
2023-11-26 15:52:39 +0100

    wooow

--- Zadání ZAL-03.py
+++ Zadání ZAL-03.py
@@ -1,8 +1,20 @@
 from math import isclose
-from math import sqrt
 
 od = int(input("Zadejte M: "))
 do = int(input("Zadejte N: "))
 
-while od > 1:
-    
+jeprvo = True
+while od >= 0:
+    if (od % 2 != 0) and (od % 3 != 0) and (od % 5 != 0) and (od % 7 != 0) and od != 1:
+        print(od)
+    if od == 2:
+        print(od)
+    if od == 3:
+        print(od)
+    if od == 5:
+        print(od)
+    if od == 7:
+        print(od)
+    od = od + 1
+    if od > do:
+        break
