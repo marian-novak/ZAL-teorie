5a2ff83e8d7543e51aa411d39116c56695054367
marian-novak <marian.novak.023@pslib.cz>
2023-11-29 09:38:39 +0100

    pro

--- Zadání ZAL-03.py
+++ Zadání ZAL-03.py
@@ -3,16 +3,15 @@ from math import sqrt
 od = int(input("Zadejte N: "))
 do = int(input("Zadejte M: "))
 
-cislo = od
-while cislo <= do:
-    if cislo >= 2:
+while od <= do:
+    if od >= 2:
         je_prvocislo = True
         i = 2
-        while i <= sqrt(cislo):
-            if cislo % i == 0:
+        while i <= sqrt(od):
+            if od % i == 0:
                 je_prvocislo = False
                 break
             i = i + 1
         if je_prvocislo:
-            print(cislo)
-    cislo = cislo + 1
+            print(od)
+    od = od + 1
---
+++ lekce9-procvicovani.py
@@ -0,0 +1,14 @@
+
+#i = 1
+#while i < 10:
+    #print("ja jsem upir dex")
+    #i *= 2
+#print("hafo")
+
+for x in range(0, 10): # pro "index" v rozsahu "0 až 6" opakuj
+    if x == 4:
+        print("mooo")
+    elif x % 2 == 0:
+        print("meow")
+    else:
+        print("haf")
