1f3d9fba2b2e49bbf3cf18838c868c68f4623f97
marian-novak <marian.novak.023@pslib.cz>
2023-12-10 15:47:07 +0100

    lol

--- Zadání ZAL-04.py
+++ Zadání ZAL-04.py
@@ -2,4 +2,14 @@ rozmer = int(input("Rozmery pole: "))
 
 pole2d = [ [0]*rozmer for i in range(rozmer) ]
 
-print(pole2d[1][4])
+if rozmer < 3 or rozmer > 27:
+    print("Chyba, moc velke nebo male pole.")
+
+while rozmer >= 3 and rozmer <= 27:
+    Xzadanex = int(input(""))
+    Xzadaney = int(input(""))
+    pole2d [Xzadanex][Xzadaney] = "X"
+    Yzadanex = int(input(""))
+    Yzadaney = int(input(""))
+    pole2d [Yzadanex][Yzadaney] = "Y"
+print(pole2d[2][2])
