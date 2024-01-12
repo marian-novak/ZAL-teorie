1addb185c696c1f3422ad46c45e9320c6bf474c7
marian-novak <marian.novak.023@pslib.cz>
2024-01-12 14:50:24 +0100

    id fuvk it

--- Zadání ZAL-04.py
+++ Zadání ZAL-04.py
@@ -1,18 +1,98 @@
+def kontrolaPole (x, y, pole2d):
+    if(x > len(pole2d) or y > len(pole2d) or x <= 0 or y <= 0):
+        return False
+    else:
+        return True
+
+def kontrolaStejne (x, y, pole2d):
+    if(pole2d[x - 1][y - 1] == 0):
+        return True
+    else:
+        return False
+
+def kontrolaVstupu(cislo):
+    try:
+        cislo = int(cislo)
+        return True
+    except ValueError: return False
+
+def zjisti_delku_posloupnosti(pole2d, symbol):
+
+    radky = len(pole2d)
+    sloupce = len(pole2d[0])
+
+    def zkontroluj_posloupnosti(rad, sloup, delrad, delsloup):
+        poc = 0
+        while 0 <= rad < radky and 0 <= sloup < sloupce and pole2d[rad][sloup] == symbol:
+            poc += 1
+            rad += delrad
+            sloup += delsloup
+        return poc
+
+    def zkontroluj_policko(rad, sloup):
+        smery = [(0, 1), (1, 0), (1, 1), (-1, 1)]
+        nejdelka = 0
+        for delrad, delsloup in smery:
+            nejdelka = max(nejdelka, zkontroluj_posloupnosti(rad, sloup, delrad, delsloup) + zkontroluj_posloupnosti(rad, sloup, -delrad, -delsloup) - 1)
+        return nejdelka
+
+    nejdelka = 0
+
+    for i in range(radky):
+        for j in range(sloupce):
+            if pole2d[i][j] == symbol:
+                nejdelka = max(nejdelka, zkontroluj_policko(i, j))
+
+    return nejdelka
+
 rozmer = int(input("Rozmery pole: "))
 
-pole2d = [ [0]*rozmer for i in range(rozmer) ]
+sym_X = "X"
+sym_O = "O"
 
+pole2d = [ [0]*rozmer for i in range(rozmer) ]
 if rozmer < 3 or rozmer > 27:
     print("Chyba, moc velke nebo male pole.")
-
+pocetkol = 0
 while rozmer >= 3 and rozmer < 27:
-    Xzadanex = int(input("")) - 1
-    Xzadaney = int(input("")) - 1
-    pole2d [Xzadanex][Xzadaney] = "X"
-    print(Xzadanex, Xzadaney)
-    Ozadanex = int(input("")) - 1
-    Ozadaney = int(input("")) - 1 
-    pole2d [Ozadanex][Ozadaney] = "O"
-    print(Ozadanex, Ozadaney)
-print(pole2d[2][2])
-
+    if((pocetkol+2) % 2 == 0):
+        zadanex = input("")
+        if zadanex == "konec":
+            break
+        if (kontrolaVstupu(zadanex)):
+            zadanex = int(zadanex)
+        zadaney = input("")
+        if zadaney == "konec":
+            break
+        if (kontrolaVstupu(zadaney)):
+            zadaney = int(zadaney)
+        if (kontrolaPole(zadanex, zadaney, pole2d) == False):
+            print("Tah je mimo pole.")
+            break
+        if (kontrolaStejne(zadanex, zadaney, pole2d) == False):
+            print("Tah uz je v poli.")
+            break
+        pole2d [zadanex - 1][zadaney - 1] = "X"
+        delka_posloupnosti_X = zjisti_delku_posloupnosti(pole2d, sym_X)
+        print("Hrac'X': nejdelsi", delka_posloupnosti_X)
+    else:
+        zadanex = input("")
+        if zadanex == "konec":
+            break
+        if (kontrolaVstupu(zadanex)):
+            zadanex = int(zadanex)
+        zadaney = input("")
+        if zadaney == "konec":
+            break
+        if (kontrolaVstupu(zadaney)):
+            zadaney = int(zadaney)
+        if (kontrolaPole(zadanex, zadaney, pole2d) == False):
+            print("Tah je mimo pole.")
+            break
+        if (kontrolaStejne(zadanex, zadaney, pole2d) == False):
+            print("Tah uz je v poli.")
+            break
+        pole2d [zadanex - 1][zadaney - 1] = "O"
+        delka_posloupnosti_O = zjisti_delku_posloupnosti(pole2d, sym_O)
+        print("Hrac'O': nejdelsi", delka_posloupnosti_O)
+    pocetkol += 1
--- čede.py
+++ čede.py
@@ -1,42 +1,38 @@
 def zjisti_delku_posloupnosti(pole, symbol):
-    # Funkce přijímá 2D pole a symbol (X nebo O) a vrací délku nejdelší posloupnosti symbolů.
 
-    rows = len(pole)
-    columns = len(pole[0])
+    radky = len(pole)
+    sloupce = len(pole[0])
 
     def zkontroluj_posloupnosti(r, c, dr, dc):
-        # Pomocná funkce pro kontrolu posloupností ve směru (dr, dc).
         count = 0
-        while 0 <= r < rows and 0 <= c < columns and pole[r][c] == symbol:
+        while 0 <= r < radky and 0 <= c < sloupce and pole[r][c] == symbol:
             count += 1
             r += dr
             c += dc
         return count
 
     def zkontroluj_policko(r, c):
-        # Kontrola posloupností z aktuálního políčka ve všech směrech.
-        directions = [(0, 1), (1, 0), (1, 1), (-1, 1)]  # Všechny směry: vodorovně, svisle, diagonálně.
+        directions = [(0, 1), (1, 0), (1, 1), (-1, 1)]
         max_length = 0
         for dr, dc in directions:
             max_length = max(max_length, zkontroluj_posloupnosti(r, c, dr, dc) + zkontroluj_posloupnosti(r, c, -dr, -dc) - 1)
         return max_length
 
-    max_length = 0
+    nejdelka = 0
 
-    # Projít všechna políčka a zkontrolovat délku posloupností.
-    for i in range(rows):
-        for j in range(columns):
+    for i in range(radky):
+        for j in range(sloupce):
             if pole[i][j] == symbol:
-                max_length = max(max_length, zkontroluj_policko(i, j))
+                nejdelka = max(nejdelka, zkontroluj_policko(i, j))
 
-    return max_length
+    return nejdelka
 
 # Příklad použití:
 pole = [
     ['X', 'O', 'O', 'O', 'X'],
     ['X', 'X', 'O', 'X', 'O'],
     ['O', 'O', 'X', 'X', 'X'],
-    ['X', 'O', 'X', 'O', 'O']
+    ['X', 'X', 'X', 'O', 'O']
 ]
 
 symbol_X = 'X'
